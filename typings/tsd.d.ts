13499da8ecc1698500813dee2f947213472615ca
Andy Hanson <anhans@microsoft.com>
2017-02-25 15:46:35 -0800

    Update from master

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="typeahead/typeahead.d.ts" />
-/// <reference path="handlebars/handlebars.d.ts" />
-
--- update-data.cmd
+++
@@ -1,3 +0,0 @@
-copy /y ..\types-publisher\data\search-index-full.json .
-copy /y ..\types-publisher\data\search-index-head.json .
-copy /y ..\types-publisher\data\search-index-min.json .
